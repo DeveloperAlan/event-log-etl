3275ad1501cd7d81976699b3644b227b9a8e0256
Alan Ng <alanng95@hotmail.com>
2024-03-11 12:09:01 +1100

    fix: add cors package and cors option so client can talk to backend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,7 +1,15 @@
 import express from 'express';
+import cors  from 'cors';
 import eventsRouter from './routes';
 
 const app = express();
+
+const corsOptions = {
+    origin: 'http://localhost:3000',
+    optionsSuccessStatus: 200,
+  };
+
+app.use(cors(corsOptions))
 app.use('/events', eventsRouter);
 
 export default app;
